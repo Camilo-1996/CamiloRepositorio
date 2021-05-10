b319cdca37a9b3b5213de4beec00e39838420a46
Melani Ramos Lopez <melani.ramos@educa.madrid.org>
2021-05-11 01:24:33 +0200

    Prueba error no me compila eclipse. metodo solo_ida a medio inicio

--- src/Billete.java
+++ src/Billete.java
@@ -1,17 +1,33 @@
 
+import java.time.*;
+import java.util.Scanner;
 public class Billete {
 
 	private String origen ;
 	private String destino;
 	//private Date fechaSalida;
 	private Persona pasajero;
-	
+	private Localdate fechas;
 	// data base o fichero
 	
 	//esta clase genera un fichero txt
 	
-
-	fdfgfdfgdf
-
-	
+	//Metodo de busqueda
+	public void solo_ida (Persona pasajero) {
+		
+	}
+	_
+	public static void menu() {
+		public void solicito_datos(Scanner nc) {
+			String fechas = "";
+			System.out.println("Introduce la dia/mes/año de salida");
+			for (int i = 0; i < fechas.length; i++) {
+				
+			}
+			//Excepcion de NumberformatException
+			
+			
+			
+		}
+	}
 }
---
+++ src/Pruebas.java
@@ -0,0 +1,9 @@
+package clases_borrador;
+
+public class Ventas {
+	public static void main (String [] args) {
+		
+		
+	}
+
+}
