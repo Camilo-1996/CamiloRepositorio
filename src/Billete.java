881be73e0bd5762cd5ab1682b142a8b28276c1f9
Melani Ramos <melaniramoslopez@gmail.com>
2021-05-10 10:48:19 +0200

    cambio de clase vuelos ---> Billetes

---
+++ src/Billete.java
@@ -0,0 +1,17 @@
+
+public class Billete {
+
+	private String origen ;
+	private String destino;
+	//private Date fechaSalida;
+	private Persona pasajero;
+	
+	// data base o fichero
+	
+	//esta clase genera un fichero txt
+	
+
+	
+
+	
+}
