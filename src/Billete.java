8ea3a681bbad6164fb5c9df7f55d201d6da82830
Dam1 <Dam1@DESKTOP-L846TEE>
2021-05-11 14:02:06 +0200

    El billete solo sera un objeto de consulta y cambio (get y set)

--- src/Billete.java
+++ src/Billete.java
@@ -1,34 +1,13 @@
-
 import java.time.*;
-import java.util.Scanner;
 public class Billete {
 
-	private String origen ;
-	private String destino;
-	//private Date fechaSalida;
-	private Persona pasajero;
-	private Localdate fechas;
-	// data base o fichero
-	
 	//esta clase genera un fichero txt
 	
-	//Metodo de busqueda
-	public void solo_ida (Persona pasajero) {
-		
-	}
-	_
-	public static void menu() {
-		public void solicito_datos(Scanner nc) {
-			String fechas = "";
-			System.out.println("Introduce la dia/mes/año de salida");
-			for (int i = 0; i < fechas.length; i++) {
-				
-			}
-			//Excepcion de NumberformatException
-			
-			int j=0;
-			
-			
-		}
-	}
+	private Persona pasajero;
+	private String origen ;
+	private String destino;
+	private Date fechas;
+	
+	
+	
 }
