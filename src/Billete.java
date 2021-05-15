517ca1f2251c2efc9b15cead506113edfdc7d21c
Melani Ramos Lopez <melani.ramos@educa.madrid.org>
2021-05-15 18:17:55 +0200

    hola prueba

--- src/Billete.java
+++ src/Billete.java
@@ -1,8 +1,19 @@
 import java.time.*;
 public class Billete {
 
+<<<<<<< Upstream, based on branch 'master' of https://github.com/Camilo-1996/ProyectoFinal.git
+=======
+	private String origen ;
+	private String destino;
+	//private Date fechaSalida;
+	private Persona pasajero;
+	//private Localdate fechas;
+	// data base o fichero
+	
+>>>>>>> 7712d92 Corrigiendo errores
 	//esta clase genera un fichero txt
 	
+<<<<<<< Upstream, based on branch 'master' of https://github.com/Camilo-1996/ProyectoFinal.git
 	private Persona pasajero;
 	private String origen ;
 	private String destino;
@@ -11,3 +22,17 @@ public class Billete {
 	private Persona prueba;
 	
 }
+=======
+	//Metodo de busqueda
+	public void solo_ida (String destino) {
+		
+	}
+	public static void menu() {
+			String fechas = "";
+			System.out.println("Introduce la dia/mes/año de salida");
+			//Excepcion de NumberformatException
+
+		}
+
+}
+>>>>>>> 7712d92 Corrigiendo errores
---
+++ src/Pruebas.java
@@ -0,0 +1,11 @@
+package clases_borrador;
+
+public class Ventas {
+	public static void main (String [] args) {
+		
+		String fechas = "";
+		System.out.println("Introduce la dia/mes/año de salida");
+
+	}
+
+}
