703d5fd62469b69dd80f868b4fe37c3be44960a3
Melani Ramos <melaniramoslopez@gmail.com>
2021-05-20 13:58:50 +0200

    Cambio de estructura, en main y Gestion

--- src/gestion/Gestion.java
+++ src/gestion/Gestion.java
@@ -1,17 +1,37 @@
 package gestion;
 
 public class Gestion {
+	Reservas reservas = new Reservas();
+	
+	public void opcion(int opcion) {
+		
+		switch(opcion) {
+		case 1: 
+			soloIda();
+		case 2:
+			idayVuelta();
+		case 3:
+			
+		}
+	}
 	
 	/*Gestion de datos Vuelos*/
 	public void gestionarDestino() {
 		
-	
+		
 	}
 	
 	public void gestionarPartida() {
 		
 	}
 	
+	public void soloIda() {
+		
+	}
+	
+	public void idayVuelta() {
+		
+	}
 	/*Gestion datos*/
 
 }
