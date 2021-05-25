78a9e9642740f786e5ff80aa12e553b5e7b68313
Melani Ramos <melaniramoslopez@gmail.com>
2021-05-25 14:20:19 +0200

    añado metodo de busqueda y consulta Hotel

--- src/gestion/Gestion.java
+++ src/gestion/Gestion.java
@@ -1,9 +1,13 @@
 package gestion;
 
-import java.util.*;
+import java.sql.Date;
+import java.time.LocalDate;
 
+import billete.Persona;
+import gestion.BBDDtest;
 import reserva.Hotel;
-import reserva.Reservas;
+
+import java.util.*;
 
 public class Gestion {
 	Scanner scn=new Scanner(System.in);
@@ -67,13 +71,23 @@ public class Gestion {
 
 	/*Operaciones y metodos de la clase Hoteles*/
 	public void buscoDestinosHoteles(String destino) {
-		
-		
-		Hotel h1= new Hotel(null, null, destino, 0); 
+		Date entrada; 
+		Date salida; 
+		Hotel h1= new Hotel(entrada, salida, destino, 0); 
 		//El destino tiene como resultado 
 	if(destino.equalsIgnoreCase(destino)) {
 		System.out.println("Introduce la fecha de entrada");
-		
+		int dia = scn.nextInt();
+		h1.setDia(dia);
+		int mes = scn.nextInt();
+		h1.setMes(mes);
+		h1.setFecha_entrada(new Date(dia, mes, h1.getAño()));
+		System.out.println("Introduce la fecha de salida 'dia/mes'");
+		int dia1 = scn.nextInt();
+		System.out.println("");
+		int mes1 = scn.nextInt();
+		h1.setFecha_salida(new Date(dia1, mes1, h1.getAño()));
+	
 	
 	}
 	}
