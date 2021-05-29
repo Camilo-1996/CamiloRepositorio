7ab11bee48b4e988cd2ae116d56a77b854b854fd
Familia <Familia@DESKTOP-VF874F2>
2021-05-29 02:02:33 +0200

    restauración 2

---
+++ src/billete/Billete.java
@@ -0,0 +1,33 @@
+package billete;
+
+public class Billete {
+
+	//esta clase genera un fichero txt
+
+	private Persona datosPasajero;
+	private static long numeroBillete;
+	private Vuelo datosVuelo;
+	private long nBillete;
+	
+	public Billete (Persona persona, Vuelo vuelo) {
+		
+		this.datosPasajero=persona;
+		this.datosVuelo=vuelo;
+		
+		nBillete=numeroBillete++;
+		
+	}
+	
+	
+	//método para consultar los datos del billete
+	public void datosBillete() {
+		
+		System.out.println("DATOS BILLETE Nº "+ nBillete);
+			
+		//falta poner los otros datos persona y Vuelo
+	}
+	
+	
+
+	
+}
---
+++ src/billete/Persona.java
@@ -0,0 +1,78 @@
+package billete;
+
+public class Persona {
+
+	private String nombre;
+	private String apellido1;
+	private String apellido2;
+	private int DNI;
+	private String pasaporte;
+	
+
+
+	// constructor
+	public Persona(String nombre, String apellido1, String apellido2, String pasaporte) {
+		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.apellido2=apellido2;
+		this.pasaporte = pasaporte;
+		
+	}
+	// fin constructor
+
+	/*
+	 *Getters and Setters
+	 */
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido1() {
+		return apellido1;
+	}
+
+	public void setApellido1(String apellido) {
+		this.apellido1 = apellido;
+	}
+	
+	public String getApellido2() {
+		return apellido2;
+	}
+
+	public void setApellido2(String apellido) {
+		this.apellido2 = apellido;
+	}
+
+	public int getDNI() {
+		return DNI;
+	}
+
+	public void setDNI(int dNI) {
+		DNI = dNI;
+	}
+
+	public String getPasaporte() {
+		return pasaporte;
+	}
+
+	public void setPasaporte(String pasaporte) {
+		this.pasaporte = pasaporte;
+	}
+
+	/*
+	 * Fin Getters and Setters
+	 *
+	 */
+	
+	
+	public String getNombreCompleto() {
+		
+		return nombre+" "+apellido1+ " "+ apellido2;
+	}
+
+}
