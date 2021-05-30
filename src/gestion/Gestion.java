1dac20e608e6552e2e847e865b59ad43fef9c706
Melani Ramos Lopez <melani.ramos@educa.madrid.org>
2021-05-30 19:47:11 +0200

    Resttauro los metodos de Gestion.

--- src/gestion/Gestion.java
+++ src/gestion/Gestion.java
@@ -6,6 +6,8 @@ import java.time.LocalDate;
 import billete.Vuelo;
 import interfazes.Menus;
 import reserva.Hotel;
+import src.billete.Billete;
+import src.modelos.Aerolinea;
 
 import java.util.*;
 
@@ -13,317 +15,291 @@ import bd.BBDDtest;
 
 public class Gestion {
 	
-	private static Scanner scn = new Scanner(System.in);
-	private Billete billete=new Billete();
+	Scanner scn = new Scanner(System.in);
+	private Billete billete = new Billete();
 
-	
-	
 	public void inicioSesion() {
-			
+
 		boolean opcionCorrecta = false;
 		int opcionElejida = 0;
-		
-		while(opcionCorrecta!=true) {
-			
+
+		while (opcionCorrecta != true) {
+
 			Menus.menuUsuario();
-			
+
 			try {
-				
+
 				opcionElejida = Integer.parseInt(scn.next());
-				
-				if(opcionElejida <= 4 && opcionElejida > 0) {
-					
+
+				if (opcionElejida <= 4 && opcionElejida > 0) {
+
 					opcionCorrecta = true;
-					
-				}else {
-					
+
+				} else {
+
 					System.out.println("NO DISPONEMOS DE ESA OPCIÓN. INTELANTALO DE NUEVO\n");
-					
+
 				}
-				
-				
-			}catch(NumberFormatException exc){
-				
-				
+
+			} catch (NumberFormatException exc) {
+
 				System.out.println("FORMATO NO VALIDO\n");
-			
-				
-			}catch(Exception exc) {
-				
+
+			} catch (Exception exc) {
+
 				System.out.println("ERROR\n");
 				exc.printStackTrace();
-				
+
 			}
-		} 
-		
-		switch(opcionElejida) {
-		
-		
-			case 1:
-				this.ida();
-				break;
-			case 2:
-				this.idaVuelta();
-				break;
-			case 3:
-				this.vueloHotel();
-				break;
-			case 4:
-				this.finApp();
-				break;
+		}
+
+		switch (opcionElejida) {
+
+		case 1:
+			this.ida();
+			break;
+		case 2:
+			this.idaVuelta();
+			break;
+		case 4:
+			this.finApp();
+			break;
 		}
 	}
-	
-	
-	
+
 	/**
-	 * Método de Ida
+	 * Método de Ida ,Que tiene como variables 
 	 */
 	public void ida() {
-		
 		String origen = "";
 		String destino = "";
-		
-		System.out.println("Introduce el origen");
-		
-		do {
-			
-			origen =scn.next();
-			
-			this.comprueboPais(origen);
-			
-			if(this.comprueboPais(origen)!=true) {
-				
-				System.out.println("INTRODUCE UN ORIGEN VALIDO.");
-			}
-			
-			
-		}while(this.comprueboPais(origen)!=true);
-		
-		
-		System.out.println("Introduce el destino");
-		
-		
-		do {
-			
-			destino = scn.next();
-			
-			this.comprueboPais(destino);
-			
-			if(this.comprueboPais(destino)!=true) {
-				
-				System.out.println("INTRODUCE UN DESTINO VALIDO.");
-			}
-			
-			
-		}while(this.comprueboPais(destino)!=true);
-		
-		this.solicitoFechas(0, 0, null);
-		
+		int diaSalida = 0;
+		int mesSalida = 0;
+		solicitoOrigenyDestino(origen,destino);
+		System.out.println("Indica la fecha de salida a " + destino);
+		comprobarDiayMes(diaSalida, mesSalida);
+		precioVueloSoloIda(billete.getFecha());
+
 	}
-	
-	
-	
-	public boolean comprueboPais(String pais) {
-		
-		boolean estaPais=false;
-	
-		
-		if(staticmetodoPaulBDcomprobarPais(String pais)!=false){
-				
-			estaPais=true;
-		}
-		
-		return estaPais;
-	}
-		
-		
-	
-	
-	
+	/**
+	 * Método de Ida y vuelta ,Que tiene como variables 
+	 */
 	public void idaVuelta() {
-		
+		String origen = "";
+		String destino = "";
+		int diaSalida = 0;
+		int mesSalida = 0;
+		int diaRetorno =0;
+		int mesRetorno =0;
+
+		solicitoOrigenyDestino(origen,destino);
+		comprobarDiayMes(diaSalida,mesSalida);
+		comprobarDiayMes(diaRetorno, mesRetorno);
+
 	}
-	
-	public void vueloHotel() {
-		String origen = "España";
-		System.out.println("Introduce origen");
-		try {
-			buscoHoteles(origen);
-		}catch(NoSuchElementException e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		}catch(Exception e) {
-			e.printStackTrace();
-			System.out.println(e.getMessage());
-		}
-	}
-	
+	/**
+	 * Método de fin de Aplicacion 
+	 */
 	public void finApp() {
 		System.out.println("HASTA LUEGO!!");
 		System.exit(0);
 	}
-	/*Gestion de datos Vuelos*/
-	
-	public void gestionarPartida() {
-		
-	}
-	
-	public void soloIda(String origen, String destino) {
-		LocalDate entrada = null; 
-		LocalDate salida = null;
-		int dia = 0;
-		int  mes = 0;
-		String compañía = "";
-		Vuelo pasajero = new Vuelo(destino, origen, compañía);
-		String fecha = new String ("111");
-		System.out.println("Introduce el origen");
-		origen = Menus.scn.next();
-		System.out.println("Introduce el destino");
-		destino = Menus.scn.next();
-		solicitoFechas(dia, mes,entrada);
-	}
-	
-	public void idayVuelta() {
-		
-		
-		
-	}
-	/*Gestion datos*/
 
-	/*Operaciones y metodos de la clase Hoteles*/
 	
-	public void buscoHoteles(String destino) {
-		LocalDate entrada = null; 
-		LocalDate salida = null;
-		int dia =0;
-		int  mes =0;
-		Hotel h1= new Hotel(entrada, salida, destino, 0); 
-		//El destino tiene como resultado
-		
-		solicitoFechas(dia, mes, entrada);
-		
-		System.out.println("Tenemos Estos Precios Para Estos Hoteles:\n");
-		precioHotel();
+	/**
+	 * metodos que comprueban Si el destino y origen introducido son el correcto,  
+	 * */
+	public  void solicitoOrigenyDestino(String origen,String destino) {
+		System.out.println("Introduce tu ciudad o pais de origen");
+
+		do {
+
+			origen = scn.next();
+
+			this.compueboCiudadoPais(origen);
+
+			if (this.compueboCiudadoPais(origen) != true) {
+
+				System.out.println("INTRODUCE UN ORIGEN VALIDO.");
+			}
+
+		} while (this.compueboCiudadoPais(origen) != true);
+
+		System.out.println("Introduce la ciudad o pais de destino");
+
+		do {
+
+			destino = scn.next();
+
+			this.compueboCiudadoPais(destino);
+
+			if (this.compueboCiudadoPais(destino) != true) {
+
+				System.out.println("INTRODUCE UN DESTINO VALIDO.");
+			}
+
+		} while (this.compueboCiudadoPais(destino) != true);
+		System.out.println("Indica el dia/mes de tu viaje");
 	
 	}
 
-	
-	
-	public void comprobarDiayMes() {
-		
-		int diaComprobado=0;
-		int mesComprobado=0;
-		
-		boolean diaCorrecto=false;
-		
-		while(diaCorrecto!=true) {
-			
-			try{
-				
-				System.out.println("DIA");
-				
-				diaComprobado=scn.nextInt();
-				
-				System.out.println("MES");
-				
-				mesComprobado=scn.nextInt();
-				
-				if((diaComprobado<31 && diaComprobado>0)&&(mesComprobado>0 && mesComprobado<13)) {
-					
-					
-					billete.setFecha(diaComprobado, mesComprobado, 2021);
-					
-					
-					
-					diaCorrecto=true;
-					
-					
-				}else {
-					
+	/*
+	 * ERROR DE REPETICION DE ESTE METODO AUNQUE SI COMPRUEBA LA CIUDAD POR RAZON
+	 * DESCONOCIDA REPITE LA OPERACION 3 VECES,(NOTA: NO ME FUNCIONA EL BICHO
+	 * DEPURAR)
+	 **/
+	public boolean eligoCiudad(String ciudadOPais) {
+		String laCiudad = "";
+		boolean estaCiudad = false;
+		System.out.println("A QUE CIUDAD DE " + ciudadOPais + " QUIERES VIAJAR");
+		// consultaPaul que devuelve las ciudades dek pais
+		System.out.println("Madrid");// prueba En ESPERA DE LA CONSULTA DE LA BBDD
+		System.out.println("Barcelona");// prueba En ESPERA DE LA CONSULTA DE LA BBDD
+		System.out.println("Valencia");// prueba En ESPERA DE LA CONSULTA DE LA BBDD
+		try {
+			System.out.println("introduce el nombre de la Ciudad");
+			laCiudad = scn.next();
+			if (laCiudad.equalsIgnoreCase("Madrid")) {
+				estaCiudad = true;
+			} else {
+				System.out.println(
+						"La Ciudad que nos indicas no tiene el Aereopuerto 0 el nombre no es correcto activo solo las mostradas");
+				System.out.println("Introduce el nombre la Ciudad nuevamente \n");
+				eligoCiudad(ciudadOPais);
+			}
+		} catch (Exception e) {
+			System.out.println("INTRODUCE EL NOMBRE CORRECTO");
+		}
+		return estaCiudad;
+	}
+
+	/*
+	 * Este metodo comprueba si lo introducido es ciudad o pais, si es ciudad es
+	 * correcto si es pais va al metodo eligoCiudad y lista las ciudades el que se
+	 * debe elegir, ya que en un pais puede hacer 1 o mas aereopuertos, pero no
+	 * todas las ciudades lo tienen
+	 * 
+	 **/
+	public boolean compueboCiudadoPais(String ciudadOPais) {
+		String elPais = "España";// prueba
+		String laciudad = "Madrid";// prueba
+		boolean estaPais = false;
+		// IF(staticmetodoPaulBDcomprobarPais(String pais))Metodo booleaan
+		if (ciudadOPais.equalsIgnoreCase(elPais)) {
+			estaPais = eligoCiudad(ciudadOPais);
+		} else {
+			if (ciudadOPais.equalsIgnoreCase(laciudad)) {
+				estaPais = true;
+			}
+
+		}
+		return estaPais;
+	}
+
+	/*
+	 * esta funcion esta en la clase PaisBBDD
+	 * 
+	 * public boolean ExisteCiudad(String ciudad) { String elPais= "Madrid";//prueba
+	 * 
+	 * boolean estaPais=false;
+	 * 
+	 * //staticmetodoPaulBDcomprobarPais(String pais)Metodo booleaan
+	 * if(elPais.equalsIgnoreCase(ciudad)){
+	 * 
+	 * estaPais=true; }
+	 * 
+	 * return estaPais; }
+	 */
+
+
+	/***************************************************************************/
+
+	public void comprobarDiayMes(int diaComprobado, int mesComprobado) {
+		LocalDate fecha = null;
+		boolean diaCorrecto = false;
+		while (diaCorrecto != true) {
+
+			try {
+
+				System.out.println("Indica el dia de tu viaje");
+
+				diaComprobado = scn.nextInt();
+
+				System.out.println("y el mes?");
+
+				mesComprobado = scn.nextInt();
+
+				if ((diaComprobado <= 31 && diaComprobado >= 1) && (mesComprobado > 0 && mesComprobado < 13)) {
+					fecha = LocalDate.of(2021, mesComprobado, diaComprobado);
+					this.billete.setFecha(fecha);// Camilo Satters Getters de la clase Billete
+					diaCorrecto = true;
+
+				} else {
+
 					System.out.println("INTRODUCE UNA FECHA VALIDA");
 					System.out.println("INTRODUCE DE NUEVO EL DIA");
-					
+
 				}
-				
-			}catch(Exception exc) {
-				
+
+			} catch (Exception exc) {
+
 				System.out.println("INTRODUCE UN FORMATO ADECCUADO");
 				System.out.println("INTRODUCE DE NUEVO EL DIA");
-				
+
 			}
 		}
-		
 
-		
 	}
-	
-	
-	
-	
-	
-	/*
-	 * Este metodo solicita las fechas al usuario introducuieno 
-	 * **/
-	//El flujo Scnner me da error al momento de solicitar las fechas
-	public void solicitoFechas(int dia, int  mes, LocalDate entrada) {
-		System.out.println("Introduce Una Fecha\n");
-		System.out.println("¿Cual Dia?");
-		try {
-			dia = Menus.scn.nextInt();
-		}catch(java.util.InputMismatchException e) {
-			System.out.println("\nIntroduzca Una Opcion Valida!!\n");
-			vueloHotel();
-		}
-		System.out.println("¿Cual Mes?");
-		mes = Menus.scn.nextInt();
-		entrada = LocalDate.of(2021, mes, dia);
-		System.out.println(entrada.toString());
-	}
-		
-	
-	/******************************************************************************************/
-	/**************************METODOS DE PRECIOS HOTEL, VUELOS*******************************/ 
-	/*
-	 * 	double precio_MEDIO = Math.random()*200 +70;
-		double precio_ALTO = Math.random()*600 +100;*/
-	
-	/*Consulta de precios*/
+
 	/**
-	 * Metodo que contiene un array en la cual se rellena de numeros aleatroios de 500 a 800 para precio de
-	 * vuelos de solo ida
-	 * */
-	public void precioHotel() {
-		double precio_Hotel = Math.random()*100 +150;
-		double [] listado_precio = new double [10];
-		String [] fechas_desde_hasta = new String [10];
-		String fecha = "111";
-		
+	 * Metodo que contiene un array en la cual se rellena de numeros aleatroios de
+	 * 500 a 800 para precio de vuelos de solo ida
+	 */
+	public void precioVueloSoloIda(LocalDate fecha) {
+		float[] listado_precio = new float[6];
+		LocalDate[] listado_Fechas = new LocalDate[6];
+		Aerolinea[] Aereolineas = new Aerolinea[6];// Falta rellenar nombres de aereolineas
+
+		System.out.println("Desde el dia " + fecha.getDayOfMonth() + " de " + fecha.getMonth()
+				+ ". Tenemos las siguientes opciones;\n");
+		System.out.println("Fecha       Precio Recomendado       Precio Solicitado");
+		System.out.println("-----       ------------------       -----------------");
 		for (int i = 0; i < listado_precio.length; i++) {
-				listado_precio[i] = precio_Hotel;
-				System.out.print(" "+listado_precio[i]);	
-		}
-	}
-	/**
-	 * Metodo que contiene un array en la cual se rellena de numeros aleatroios de 500 a 800 para precio de
-	 * vuelos de solo ida
-	 * */
-	public void precioVueloSoloIda() {
-		double precio_VueloSoloIda = Math.random()*500 +800;
-		double [] listado_precio = new double [10];
-		String [] fechas_desde_hasta = new String [10];
-		
-		for (int i = 0; i < listado_precio.length; i++) {
-			listado_precio[i] = precio_VueloSoloIda;
-			System.out.print(" "+listado_precio[i]);
+			float precio_VueloSoloIda_Medio = (float) (Math.random() * 300 + 700);
+			listado_precio[i] = precio_VueloSoloIda_Medio;
+			listado_Fechas[i] = fecha.of(fecha.getYear(), fecha.getMonthValue(), fecha.getDayOfMonth() + i);
+			System.out.print(listado_Fechas[i] + " ->");
+			System.out.print(i + 1 + "a - " + listado_precio[i] + "€");
+			float precio_VueloSoloIda_Alto1 = (float) (Math.random() * 600 + 900);
+			listado_precio[i] = precio_VueloSoloIda_Alto1;
+			System.out.print("     |    " + i + 1 + "b - " + listado_precio[i] + "€\n");
+
 		}
 	}
 
-	
+	// sOLO SE DEBE DEVOLVER EN UNA MATRIZ JUNTO CON LAS FECHAS DE SALIDA Y LLEGADA
+	// PERO LO DEMAS ES LO MISMO
+	public void precioVueloIdayVuelta(LocalDate fecha) {
+		float[] listado_precio = new float[6];
+		LocalDate[] listado_Fechas = new LocalDate[6];
+		Aerolinea[] Aereolineas = new Aerolinea[6];// Falta rellenar nombres de aereolineas
+		// MATRIZ
+		System.out.println("Desde el dia " + fecha.getDayOfMonth() + " de " + fecha.getMonth()
+				+ ". Tenemos las siguientes opciones;\n");
+		System.out.println("Fecha       Precio Recomendado       Precio Solicitado");
+		System.out.println("-----       ------------------       -----------------");
+		for (int i = 0; i < listado_precio.length; i++) {
+			float precio_VueloSoloIda_Medio = (float) (Math.random() * 400 + 700);
+			listado_precio[i] = precio_VueloSoloIda_Medio;
+			listado_Fechas[i] = fecha.of(fecha.getYear(), fecha.getMonthValue(), fecha.getDayOfMonth() + i);
+			System.out.print(listado_Fechas[i] + " ->");
+			System.out.print(i + 1 + "a - " + listado_precio[i] + "€");
+			float precio_VueloSoloIda_Alto1 = (float) (Math.random() * 600 + 900);
+			listado_precio[i] = precio_VueloSoloIda_Alto1;
+			System.out.print("     |    " + i + 1 + "b - " + listado_precio[i] + "€\n");
+
+		}
+	}
+
 }
-	
-	
-
-
-
 	
