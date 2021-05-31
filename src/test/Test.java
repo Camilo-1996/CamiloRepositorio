f8633cba959044395b7b42446721ac96d984ba8b
Familia <Familia@DESKTOP-VF874F2>
2021-05-31 21:11:15 +0200

    Documentación de clase Test del paquete test.

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,6 +3,12 @@ package test;
 import gestion.Gestion;
 import interfazes.Menu;
 
+/**
+ * Clase que hace el test de la aplicación. La clase simplemente genera un objeto gestión
+ * para realizar la consulta del usuario, apartir de ahi se distribuye por el resto de clases del programa.
+ * @author Juan Camilo Ramírez Zapata.
+ *
+ */
 public class Test {
 
 	public static void main(String[] args) {
